9aaf7565fa4777dcdd61fa98e7224b9443d97df5
thekayshawn <kashanahmad.me@gmail.com>
2025-10-21 19:47:35 +0500

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Qrocs</title>
+	<meta name="description" content="" />
+</svelte:head>
+
+{@render children?.()}
